55deaeb0f3f0389726f68632b38c8b418d98bc3c
AleksanderSkubala <olek_s@wtvk.pl>
2018-11-02 19:01:32 +0100

    Add example in php nd js(wip)

--- example/index.js
+++ example/index.js
@@ -1,20 +1,24 @@
-    var data = `
+var data = `
     <div>Hej</div>
-    `;
-    var name = 'konspekt';
-    var down = 'download';
+`;
+var name = 'konspekt';
+var down = 'DOWNLOAD';
 
 const ob = {
-    'name' : "konspekt",
-    'down' : "load",
-    'data' : "<div>Hej</div>",
+    'name' : name,
+    'down' : down,
+    'data' : '<div>Hej</div>',
 };
 
-$.ajax({
-    url : 'http://localhost/kptApi/php/index.php',
-    method: 'get',
-    contentType : 'application/json',
-    data : ob
-}).done(function(json) {
-    console.log(json);
-});
+
+function downloadPDF(){
+    $.ajax({
+        url : 'http://localhost/kptApi/php/index.php',
+        method: 'get',
+        contentType : 'application/json',
+        data : ob
+    }).done(function(json) {
+        download(json, name+'.pdf');
+        console.log(json);
+    });
+}
